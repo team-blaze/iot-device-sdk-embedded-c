c515fb597d4533ca8dfadc16917612bb216d422b
kwokhei <kwokheiwong@github.com>
2019-03-26 16:49:40 +0000

    updated the build_lib.sh and files to support library with FreeRTOS features

--- build_lib.sh
+++ build_lib.sh
@@ -1,25 +1,57 @@
 #!/bin/sh
 
+print_help_exit() {
+    echo "Usage: "$0" debug|release [rtos||bare]"
+    echo "[debug] to build for debug or [release] for release"
+    echo "[rtos] or [] to build for FreeRTOS or [bare] for baremetal"
+    exit
+}
+
+copy_all_files() {
+    cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_rng_crypto.c src/bsp/platform/bilsv/iotc_bsp_rng_crypto_bilsv.c
+    cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_time.c src/bsp/platform/bilsv/iotc_bsp_time_bilsv.c
+    cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_io_net.c src/bsp/platform/bilsv/iotc_bsp_io_net_bilsv.c
+    cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_mem.c src/bsp/platform/bilsv/iotc_bsp_mem_bilsv.c
+}
+
+copy_freertos_files() {
+    # will overwrite the previous copied files
+    cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_mem_freertos.c src/bsp/platform/bilsv/iotc_bsp_mem_bilsv.c
+    cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_io_net_freertos.c src/bsp/platform/bilsv/iotc_bsp_io_net_bilsv.c
+}
+
+# Wipe the folder clean to avoid clashing FreeRTOS with non-FreeRTOS c files from previous run
+rm -rf src/bsp/platform/bilsv
 # We have to ensure the bilsv directory exist before running make
 mkdir -p src/bsp/platform/bilsv
 
-# cp ../bilsv3-libraries/internal/iotc_bsp/* src/bsp/platform/bilsv/
-
-cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_rng_crypto.c src/bsp/platform/bilsv/iotc_bsp_rng_crypto_bilsv.c
-cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_time.c src/bsp/platform/bilsv/iotc_bsp_time_bilsv.c
-cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_io_net.c src/bsp/platform/bilsv/iotc_bsp_io_net_bilsv.c
-cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_mem.c src/bsp/platform/bilsv/iotc_bsp_mem_bilsv.c
-
-# cp ../bilsv3-libraries/internal/iotc_bsp/iotc_bsp_mem_freertos.c src/bsp/platform/bilsv/iotc_bsp_mem_free_rtos_bilsv.c
-if [ $# -eq 0 ]; then
-    echo "Missing second arguement, [del] for developement or [rel] for release"
-    exit;
+# Accept 1 to 2 arguments
+if [ $# -lt 1 ] || [  $# -gt 2 ]; then
+    print_help_exit
 fi
 
-if [ "$1" = "del" ]; then
+copy_all_files
+
+# if RTOS support is required, copy the correct files and set flag for makefile
+if [ $# -eq 1 ]; then
+    copy_freertos_files
+elif [ "$2" = "rtos" ]; then
+    copy_freertos_files
+elif [ "$2" = "bare" ]; then
+    # do nothing
+    :
+else
+    echo invaid argument 2
+    print_help_exit
+fi
+
+if [ "$1" = "debug" ]; then
     make PRESET=BILSV3_DEL
-fi
-
-if [ "$1" = "rel" ]; then
+elif [ "$1" = "release" ]; then
     make PRESET=BILSV3_REL
+elif [ "$1" = "clean" ]; then
+    make PRESET=BILSV3_DEL clean
+else
+    echo invalid argument 1
+    print_help_exit
 fi
